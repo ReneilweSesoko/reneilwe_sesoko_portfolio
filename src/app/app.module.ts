23d5a5c24d24595a7a70c2668b37df44b09ce99b
Reneilwe Sesoko <reneilwe.sesoko@dadevs.co.za>
2022-05-09 16:29:39 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
