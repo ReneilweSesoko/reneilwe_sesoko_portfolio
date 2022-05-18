ce40e2ad44b084a01e8a7dd7bea27439e2b77f4d
Reneilwe Sesoko <reneilwe.sesoko@dadevs.co.za>
2022-05-18 15:50:41 +0200

    cv page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CurriculmVitaeComponent } from './components/curriculm-vitae/curriculm-vitae.component';
+import { CurriculmVitaeComponent } from './pages/curriculm-vitae/curriculm-vitae.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CurriculmVitaeComponent } from './components/curriculm-vitae/curriculm-vitae.component';
+import { CurriculmVitaeComponent } from './pages/curriculm-vitae/curriculm-vitae.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
